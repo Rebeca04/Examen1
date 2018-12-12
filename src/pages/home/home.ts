60d7a5ac7fc101d466cd9ee44bf053e0c0e875f8
unknown <dam204@ACARBALLEIRA.LOCAL>
2018-12-12 14:42:27 +0100

    Boton 5 Hecho

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { ListaPage } from './../lista/lista';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 
 @Component({
@@ -9,11 +9,11 @@ import { ToastController } from 'ionic-angular';
 })
 export class HomePage {
 
-  contador : number = 0;
+  public contador : number = 1;
   mostrarB4 : boolean = false;
 
   paginaLista: any = ListaPage;
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public loadingCtrl: LoadingController) {
 
   }
 
@@ -31,4 +31,12 @@ export class HomePage {
     toast.present();
   }
 
+  boton5(){
+    this.navCtrl.push(ListaPage);
+    const loader = this.loadingCtrl.create({
+      content: "Cargando ...",
+      duration: 2000
+    });
+    loader.present();
+  }
 }
